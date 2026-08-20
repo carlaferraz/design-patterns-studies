68841efea4b17867ec8f6427a7dd96561b406443
Carla Ferraz <carlafzaraujo@gmail.com>
2026-08-20 14:48:32 -0300

    fix: updating homework

--- design-patterns/src/Main.java
+++ design-patterns/src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         usuarios.add(new User(102L, "Ana Maria Silva", "ana.silva@email.com", "98765432100"));
         usuarios.add(
                 new User(103L, "João Pedro de Alcântara Bragança", "joao.pedro@email.com", "45678912345"));
-        usuarios.add(new User(104L, "Mariana Costa", "marianacosta.email.com", "11122233344"));
+        usuarios.add(new User(104L, "Mariana  Costa", "marianacosta.email.com", "11122233344"));
         usuarios.add(new User(105L, "Lucas Mendes", "lucas@email.com", "12345"));
         usuarios.add(new User(106L, "", "beatriz@email.com", "55566677788"));
 
